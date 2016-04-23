ac4029779efc40066a8c5bf2f46437b906eef7e4
Anuraag <baishya.anuraag1920@gmail.com>
2016-04-23 22:25:51 +0530

    Adapters

--- app/src/main/java/com/appex/edhelp/Adapters/CollegesAdapter.java
+++ app/src/main/java/com/appex/edhelp/Adapters/CollegesAdapter.java
@@ -1,23 +1,50 @@
-package com.appex.edhelp;
+package com.appex.edhelp.Adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.appex.edhelp.Activity.CollegeInfoActivity;
+import com.appex.edhelp.Models.College;
+import com.appex.edhelp.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by saketh on 23/4/16.
  */
 public class CollegesAdapter extends RecyclerView.Adapter<CollegesAdapter.ViewHolder> {
 
+    Context mContext;
+    ArrayList<College> collegeArrayList;
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.college_brief_layout,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+
+        final College college = collegeArrayList.get(position);
+        holder.collegeNameTextView.setText(college.getName());
+        holder.addressTextView.setText(college.getLocation());
+        holder.contactTextView.setText(college.getContact());
+        holder.cardView.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(mContext, CollegeInfoActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    }
+                }
+        );
 
     }
 
