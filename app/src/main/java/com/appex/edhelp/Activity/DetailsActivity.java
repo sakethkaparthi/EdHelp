88ff7410c337c0e6a1b8c95854619b44b9b7bbb4
Anuraag <baishya.anuraag1920@gmail.com>
2016-04-24 02:25:54 +0530

    College Details

--- app/src/main/java/com/appex/edhelp/Activity/DetailsActivity.java
+++ app/src/main/java/com/appex/edhelp/Activity/DetailsActivity.java
@@ -1,10 +1,13 @@
 package com.appex.edhelp.Activity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.appex.edhelp.Models.College;
 import com.appex.edhelp.R;
@@ -35,6 +38,35 @@ public class DetailsActivity extends AppCompatActivity {
         Glide.with(this).load(college.getImage())
                 .into(imageView);
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        Resources res = getResources();
+        TextView branchesTextView = (TextView) findViewById(R.id.college_branches);
+        TextView feesTextView = (TextView) findViewById(R.id.college_fees);
+        TextView applicationTextView = (TextView) findViewById(R.id.college_application);
+        TextView deadlineTextView = (TextView) findViewById(R.id.college_deadline);
+        TextView websiteTextView = (TextView) findViewById(R.id.college_website);
+
+        CharSequence branches = Html.fromHtml(String.format(res.getString(R.string.branches)));
+        branchesTextView.setText(branches);
+
+        CharSequence fees = Html.fromHtml(String.format(res.getString(R.string.fees),Math.round(college.getFees())));
+        feesTextView.setText(fees);
+
+        CharSequence application = Html.fromHtml(String.format(res.getString(R.string.application),college.getIsMains()));
+        applicationTextView.setText(application);
+
+        CharSequence deadline = Html.fromHtml(String.format(res.getString(R.string.deadline),college.getDeadline()));
+        deadlineTextView.setText(deadline);
+
+        CharSequence website = Html.fromHtml(String.format(res.getString(R.string.website)));
+        websiteTextView.setText(website);
+
+        websiteTextView.append(
+                Html.fromHtml("<a href=\""+college.getWebsite()+"\">"+college.getName()+"</a>")
+        );
+
+        String branchList[] = college.getBranches().split(",");
+        for(String branch : branchList)
+            branchesTextView.append("\n"+branch);
 
 
     }
