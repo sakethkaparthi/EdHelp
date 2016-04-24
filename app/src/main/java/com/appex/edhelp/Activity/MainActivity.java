30c18cad9049513b8c2be6ad4c0e39fc168c9ee6
sakethkaparthi <sakethkaparthi@outlook.com>
2016-04-24 12:13:29 +0530

    font changes

--- app/src/main/java/com/appex/edhelp/Activity/MainActivity.java
+++ app/src/main/java/com/appex/edhelp/Activity/MainActivity.java
@@ -186,6 +186,15 @@ public class MainActivity extends AppCompatActivity {
         Volley.newRequestQueue(getApplicationContext()).add(request);
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
     private void display() {
 
         mRecyclerView.setHasFixedSize(true);
